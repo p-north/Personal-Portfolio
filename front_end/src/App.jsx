c697d1113cbb007a8276d868b54e8f0c3c877b36
Puneet North <puneetnorth170@gmail.com>
2024-10-24 12:16:56 -0700

    configured react router for page handeling

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,3 +1,9 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Projects from "./Pages/Projects"
+import Contact from "./Pages/Contact"
+import Resume from "./Pages/Resume"
 
 
 function App() {
@@ -5,6 +11,14 @@ function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element = {<Home/>}/>
+      <Route path="/about" element = {<About/>}/>
+      <Route path="/projects" element = {<Projects/>}/>
+      <Route path="/contact" element = {<Contact/>}/>
+      <Route path="/resume" element = {<Resume/>}/>
+
+    </Routes>
       
     </>
   )
