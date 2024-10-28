ae26e7e5a0740d03a2c34e5ac8537fb894dc5458
Puneet North <puneetnorth170@gmail.com>
2024-10-27 18:32:50 -0700

    parsed personal resume using react-pdf as per #9

--- front_end/src/Pages/Resume.jsx
+++ front_end/src/Pages/Resume.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ResumeHolder from '../components/Resume/ResumeHolder'
 
 const Resume = () => {
   return (
     <div className='container mx-auto px-4'>
-      Resume Page
+      <ResumeHolder/>
     </div>
   )
 }
