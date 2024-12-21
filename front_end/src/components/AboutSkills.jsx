67ccc61d64182ff420e1c0fdfdb3a444afe21aab
p-north <puneetnorth170@gmail.com>
2024-12-21 13:59:01 -0800

    add new projects and update homepage

--- front_end/src/components/AboutSkills.jsx
+++ front_end/src/components/AboutSkills.jsx
@@ -1,16 +1,5 @@
 import React from 'react'
-import { FaPython } from "react-icons/fa";
-import { SiCplusplus } from "react-icons/si";
-import { IoLogoJavascript } from "react-icons/io5";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3 } from "react-icons/fa";
-import { FaReact } from "react-icons/fa";
-import { DiNodejs } from "react-icons/di";
-import { SiExpress } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { FaGitAlt } from "react-icons/fa6";
-import { FaNpm } from "react-icons/fa";
-import { VscVscode } from "react-icons/vsc";
+
 
 
 const AboutSkills = () => {
@@ -21,132 +10,85 @@ const AboutSkills = () => {
             <h1 className='text-3xl font-bold  md:text-4xl'>Development Skills</h1>
         </div>
         {/* Grid with icons */}
-        <div className='flex justify-center'>
+        <div className='flex justify-center scale-90'>
         <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8'>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 <img src="https://skillicons.dev/icons?i=python" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Python</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=cpp" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>C++</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=c" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>C</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=js" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>JavaScript</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=html" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>HTML</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=css" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>CSS</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=react" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>React.js</h1>
             </div>
             
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=nodejs" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Node.js</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=expressjs" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Express.js</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=tailwind" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Tailwind</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
-                <img src="https://skillicons.dev/icons?i=git" alt="cpp" className='h-20 mt-2' />
-                <h1 className='mt-4 font-semibold'>Git</h1>
+                <img src="https://skillicons.dev/icons?i=postman" alt="cpp" className='h-20 mt-2' />
+                <h1 className='mt-4 font-semibold'>Postman</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=vscode" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>VsCode</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=figma" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Figma</h1>
             </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-md cursor-pointer'>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
+                {/* <SiCplusplus className='text-8xl'/> */}
+                <img src="https://skillicons.dev/icons?i=git" alt="cpp" className='h-20 mt-2' />
+                <h1 className='mt-4 font-semibold'>Git</h1>
+            </div>
+            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-black bg-white rounded-3xl cursor-pointer'>
                 {/* <SiCplusplus className='text-8xl'/> */}
                 <img src="https://skillicons.dev/icons?i=github" alt="cpp" className='h-20 mt-2' />
                 <h1 className='mt-4 font-semibold'>Github</h1>
             </div>
             
-            
-            {/* <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <IoLogoJavascript className='text-8xl'/>
-                <h1 className='mt-1'>JavaScript</h1>
-            </div>
-            
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <FaHtml5 className='text-8xl'/>
-                <h1 className='mt-1'>HTML</h1>
-            </div>
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <FaCss3 className='text-8xl'/>
-                <h1 className='mt-1'>CSS</h1>
-            </div>
-            
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <FaReact className='text-8xl'/>
-                <h1 className='mt-1'>React.js</h1>
-            </div>
-            
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <DiNodejs className='text-8xl'/>
-                <h1 className='mt-1'>Node.js</h1>
-            </div>
-            
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <SiExpress className='text-8xl'/>
-                <h1 className='mt-1'>Express.js</h1>
-            </div>
-
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <RiTailwindCssFill className='text-8xl'/>
-                <h1 className='mt-1'>Tailwind</h1>
-            </div>
-
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <FaGitAlt className='text-8xl'/>
-                <h1 className='mt-1'>Git</h1>
-            </div>
-
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <FaNpm className='text-8xl'/>
-                <h1 className='mt-1'>Npm</h1>
-            </div>
-
-            <div className='hover:shadow-2xl p-2 border border-gray-400 flex flex-col items-center hover:scale-110 transition-transform shadow-md text-white bg-black rounded-md cursor-pointer'>
-                <VscVscode className='text-8xl'/>
-                <h1 className='mt-1'>VsCode</h1>
-            </div>
-             */}
-            
-           
-
+        
         </div>
         </div>
 
--- front_end/src/components/Projects/ProjectCard.jsx
+++ front_end/src/components/Projects/ProjectCard.jsx
@@ -1,83 +1,115 @@
-import React from 'react'
-import proj1Image from '../../assets/project1.png'
+import React from "react";
+import proj1Image from "../../assets/project1.png";
 import TransitLogo from "../../assets/project2.png";
 import JobsLogo from "../../assets/project3.jpg";
+import Mindstream from "../../assets/minstreamai.png"
 
-import { NavLink } from 'react-router-dom'
+
 
 const ProjectCard = () => {
-    const projects = [
-        {
-          id: 1,
-          date: 'Oct 2024',
-          title: "CraveIt",
-          description: "A full-stack application to locate dining options and recipes based on user preferences. Developed RESTful API endpoints with Express.js to integrate third-party APIs",
-          imag: proj1Image,
-          key1: 'React',
-          key2: 'Tailwind',
-          key3: 'Express.js',
-          link: "https://github.com/p-north/StormHacks2024_CraveIt",
-        },
-        {
-          id: 2,
-          date: 'Sept 2024',
-          title: "Transit Trip Planner",
-          description: 'Developed a Transit Trip Planner using React and integrated Google Maps API to provide users with efficient route options and real-time transit information.',
-          imag: TransitLogo,
-          key1: 'React',
-          key2: 'Tailwind',
-          key3: 'Google API',
-          link: "https://github.com/mini9yo/FallHacks",
-        },
-        {
-          id: 3,
-          date: 'Sept 2024',
-          title: "Jobs Board App",
-          description:
-            "Developed a Jobs Board using React to allow users to view, add, and edit job postings sourced from a mock JSON server! Features CRUD functionality (Create, Read, Update, Delete) for all job listings",
-          imag: JobsLogo,
-          key1: 'React',
-          key2: 'JSON Server',
-          key3: 'Tailwind',
-          link: "https://github.com/p-north/Job_Listings_Site",
-        },
-      ];
+  const projects = [
+    {
+      id: 1,
+      date: "Sept 2024",
+      title: "Mindstream AI",
+      description:
+        "Mindstream AI is an innovative web platform designed to help students efficiently generate study resources such as flashcards, quizzes, podcasts, summaries, and personalized study plans based on uploaded lecture notes.",
+      imag: Mindstream,
+      key1: "React",
+      key2: "Firebase",
+      key3: "Express.js",
+      type: "View Demo",
+      link: "https://mindstreamai.onrender.com/",
+    },
+    {
+      id: 2,
+      date: "Oct 2024",
+      title: "CraveIt",
+      description:
+        "A full-stack application to locate dining options and recipes based on user preferences. Developed RESTful API endpoints with Express.js to integrate third-party APIs",
+      imag: proj1Image,
+      key1: "React",
+      key2: "Tailwind",
+      key3: "Express.js",
+      type: "View On Github",
+      link: "https://github.com/p-north/StormHacks2024_CraveIt",
+    },
+    {
+      id: 3,
+      date: "Sept 2024",
+      title: "Transit Trip Planner",
+      description:
+        "Developed a Transit Trip Planner using React and integrated Google Maps API to provide users with efficient route options and real-time transit information.",
+      imag: TransitLogo,
+      key1: "React",
+      key2: "Tailwind",
+      key3: "Google API",
+      type: "View On Github",
+      link: "https://github.com/mini9yo/FallHacks",
+    },
+    {
+      id: 4,
+      date: "Sept 2024",
+      title: "Jobs Board App",
+      description:
+        "Developed a Jobs Board using React to allow users to view, add, and edit job postings sourced from a mock JSON server! Features CRUD functionality (Create, Read, Update, Delete) for all job listings",
+      imag: JobsLogo,
+      key1: "React",
+      key2: "JSON Server",
+      key3: "Tailwind",
+      type: "View On Github",
+      link: "https://github.com/p-north/Job_Listings_Site",
+    },
+  ];
   return (
     <div>
-        <section className='py-20 bg-gray-50'>
-        <div className='flex flex-col items-center mx-auto justify-center px-4 sm:px-6 max-w-7xl lg:px-8'>
-            <h1 className='text-3xl font-bold md:text-4xl mb-10'>My Projects</h1>
-            {/* Projects Holder */}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 scale-90'>
-                {projects.map(function(element, idx){
-
-                  return  <div key={idx} className='bg-white rounded-lg shadow-md overflow-hidden transition-transform hover:scale-105 w-full'>
-                            <img src={element.imag} alt="projectLogo" className='w-full h-48 object-cover brightness-90' />
-                            <div className='p-6 flex flex-col min-h-[240px]'>
-                                <h3 className='text-2xl font-semibold text-gray-800 mb-2'>{element.title}</h3>
-                                <p className='text-gray-500 text-sm mb-2'>{element.date}</p>
-                                <p className='text-gray-600 mb-3'>{element.description}</p>
-                                <div className='flex flex-wrap gap-2 mb-3'>
-                                <span className='text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1'>{element.key1}</span>
-                                <span className='text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1'>{element.key2}</span>
-                                <span className='text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1'>{element.key3}</span>
-
-                                </div>
-                                <div className='flex text-center bg-white hover:bg-gray-900 hover:text-white rounded-md p-3  border border-gray-200 shadow-md text-black hover:scale-105 transition-transform'>
-                                    <NavLink to ={element.link} className = 'w-full'>
-                                        View On Github
-                                    </NavLink>
-                                </div>
-                            </div>
+      <section className="py-20 bg-gray-50">
+        <div className="flex flex-col items-center mx-auto justify-center px-4 sm:px-6 max-w-7xl lg:px-8">
+          <h1 className="text-3xl font-bold md:text-4xl mb-10">My Projects</h1>
+          {/* Projects Holder */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 scale-90">
+            {projects.map(function (element, idx) {
+              return (
+                <div
+                  key={idx}
+                  className="bg-white rounded-lg shadow-md overflow-hidden transition-transform hover:scale-105 w-full"
+                >
+                  <img
+                    src={element.imag}
+                    alt="projectLogo"
+                    className="w-full h-48 object-cover brightness-90"
+                  />
+                  <div className="p-6 flex flex-col min-h-[240px]">
+                    <h3 className="text-2xl font-semibold text-gray-800 mb-2">
+                      {element.title}
+                    </h3>
+                    <p className="text-gray-500 text-sm mb-2">{element.date}</p>
+                    <p className="text-gray-600 mb-3">{element.description}</p>
+                    <div className="flex flex-wrap gap-2 mb-3">
+                      <span className="text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1">
+                        {element.key1}
+                      </span>
+                      <span className="text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1">
+                        {element.key2}
+                      </span>
+                      <span className="text-xs font-medium bg-gray-100 text-gray-700 rounded-full px-3 py-1">
+                        {element.key3}
+                      </span>
+                    </div>
+                    <div className="flex text-center bg-white hover:bg-gray-900 hover:text-white rounded-md p-3  border border-gray-200 shadow-md text-black hover:scale-105 transition-transform hover:cursor-pointer">
+                      <a href={element.link} className="w-full" target="_blank">
+                        {element.type}
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                })}
-
-            </div>
-
+              );
+            })}
+          </div>
         </div>
-        </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
